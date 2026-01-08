178047d74c613cd1e4c17f6059971d0a1a169d43
JoaoPauloAAlvim <alvesalvimj@gmail.com>
2026-01-08 16:24:26 -0300

    Teste

--- knexfile.cjs
+++ knexfile.cjs
@@ -1,6 +1,6 @@
 require("dotenv/config");
 
-const DEV_URL = (process.env.DATABASE_URL_TEST || process.env.DATABASE_URL || "").trim();
+const DEV_URL = (process.env.DATABASE_URL  || process.env.DATABASE_URL_TEST || "").trim();
 const PROD_URL = (process.env.DATABASE_URL || "").trim();
 
 module.exports = {
